97a2e23488f0c5a37d586e7a62a9ae817a84ffdc
Aaron <aaron.hsu86+github@gmail.com>
2023-07-10 01:28:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokeList, setPokemon] = useState([]);
+
+  const getPokeList = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+    .then(response => {
+      return response.json();
+    }).then(response =>{
+      // console.log(response)
+      setPokemon(response.results);
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+  // console.log(pokeList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getPokeList}>List 807 Pokemon!</button>
+      <div>
+        <ul>
+        {pokeList.length > 0 && pokeList.map((pokemon, index) => {
+          return (<li key={index+1}>{index+1} - {pokemon.name}</li>)
+        })}
+        </ul>
+      </div>
     </div>
   );
 }
